0acccdb0a3be79532eca45de4e44b433db7c574d
Rahul Sunil <rahulsunil2@gmail.com>
2020-06-07 23:16:11 +0530

    Added Categories

--- treasure_hunt/lib/home.dart
+++ treasure_hunt/lib/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:tflite/tflite.dart';
+import 'questions.dart';
+import 'dart:math';
 
 import 'camera.dart';
 
@@ -21,19 +23,23 @@ class _HomePageState extends State<HomePage> {
   String _model = "MobileNet";
   int flag = 0;
   String _label = '';
+  var questionBank = Map();
 
   @override
   void initState() {
     super.initState();
   }
 
-  loadModel() async {
+  loadModel(String category) async {
     String res = await Tflite.loadModel(
       model: "assets/mobilenet_v1_1.0_224.tflite",
       labels: "assets/mobilenet_v1_1.0_224.txt",
     );
     print(res);
     setState(() {
+      Random random = Random();
+      int randomNumber = random.nextInt(3);
+      questionBank = questionBrain[category][randomNumber];
       flag = 1;
     });
   }
@@ -43,8 +49,9 @@ class _HomePageState extends State<HomePage> {
       _recognitions = recognitions;
       _imageHeight = imageHeight;
       _imageWidth = imageWidth;
-      _label =
-          _recognitions == null ? [].toString() : _recognitions[0].toString();
+      _label = _recognitions == null
+          ? [].toString()
+          : _recognitions[0]['label'].toString();
     });
   }
 
@@ -53,11 +60,40 @@ class _HomePageState extends State<HomePage> {
     Size screen = MediaQuery.of(context).size;
     return Scaffold(
       body: flag == 0
-          ? Center(
-              child: RaisedButton(
-                child: const Text("MobileNet"),
-                onPressed: () => loadModel(),
-              ),
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: RaisedButton(
+                    child: const Text("Electronics"),
+                    onPressed: () => loadModel("Electronics"),
+                  ),
+                ),
+                Center(
+                  child: RaisedButton(
+                    child: const Text("Animals"),
+                    onPressed: () => loadModel("Animals"),
+                  ),
+                ),
+                Center(
+                  child: RaisedButton(
+                    child: const Text("Birds"),
+                    onPressed: () => loadModel("Birds"),
+                  ),
+                ),
+                Center(
+                  child: RaisedButton(
+                    child: const Text("Environment"),
+                    onPressed: () => loadModel("Environment"),
+                  ),
+                ),
+                Center(
+                  child: RaisedButton(
+                    child: const Text("Things"),
+                    onPressed: () => loadModel("Things"),
+                  ),
+                ),
+              ],
             )
           : Stack(
               children: [
@@ -66,8 +102,38 @@ class _HomePageState extends State<HomePage> {
                   _model,
                   setRecognitions,
                 ),
-                Center(
-                  child: Text(_label),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  verticalDirection: VerticalDirection.up,
+                  children: <Widget>[
+//                    SizedBox(height: 2800),
+                    Expanded(
+                      child: Container(
+                          child: Center(
+                            child: Text(_label,
+                                style: const TextStyle(
+                                    color: const Color(0xff676565),
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: "Montserrat",
+                                    fontStyle: FontStyle.normal,
+                                    fontSize: 27.0),
+                                textAlign: TextAlign.center),
+                          ),
+                          width: 500,
+                          height: 182,
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(42)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: const Color(0x29000000),
+                                    offset: Offset(0, 3),
+                                    blurRadius: 6,
+                                    spreadRadius: 0)
+                              ],
+                              color: const Color(0x22ffffff))),
+                    ),
+                  ],
                 )
               ],
             ),
---
+++ treasure_hunt/lib/questions.dart
@@ -0,0 +1,86 @@
+Map questionBrain = {
+  "Environment": [
+    {
+      "question":
+          "I am Jack not Jill, about that tropical chill. I bear the largest fruits in all the trees.",
+      "answer": "Jack fruit"
+    },
+    {
+      "question":
+          "In rage I erupt to let magma escape. Mauna Loa is the biggest of me.",
+      "answer": "Volcano "
+    },
+    {
+      "question":
+          "I am sharp and high rise rock exposure , especially on coasts and along rivers.",
+      "answer": "cliff"
+    },
+  ],
+  "Animals": [
+    {
+      "question":
+          "The largest of my type on the earth, you believe I spit fire, yet a mere reptile. I am Varanus Komodensis.",
+      "answer": "Komodo Dragon"
+    },
+    {
+      "question":
+          "With eight legs I terrorise, besides a venomous sting. Archnida skorpios, Desert aint no thang!",
+      "answer": "Scorpion"
+    },
+    {
+      "question":
+          "The Spitz in the north west Poland and Germany. Zwergspitz, we're a mere toy breed !",
+      "answer": "Pomeranian"
+    },
+  ],
+  "Birds": [
+    {
+      "question":
+          "I'm american and I sing. We assemble at night, not as batman but his enthusiastic sidekick.",
+      "answer": "Robin"
+    },
+    {
+      "question":
+          "Not to boast, yet I'm intelligent , I talk and I recognise myself in a mirror. How can I not? Im a black and white corvidae.",
+      "answer": "Magpie"
+    },
+    {
+      "question":
+          "You render me endangered. I'm white and flawless but Aron Chupa does not know to spell my name.",
+      "answer": "Albatross "
+    },
+  ],
+  "Electronics": [
+    {
+      "question":
+          "Console or PC, gamer or pilot - you control better a strand, with me in you hand.",
+      "answer": "Joystick"
+    },
+    {
+      "question":
+          "I am your portable world, the british call me a notebook. You use me in your lap and fold me when I stop.",
+      "answer": "Laptop"
+    },
+    {
+      "question":
+          "Round and round, table and above. I swallow power to give you some cold.",
+      "answer": "Electric fan"
+    },
+  ],
+  "Things": [
+    {
+      "question": "Read me or write me but dont judge me by the cover.",
+      "answer": "NoteBook"
+    },
+    {
+      "question":
+          "Im one of the oldest inventions. Hang me and watch me oscillate as time passes by.",
+      "answer": "Wall Clock"
+    },
+    {
+      "question":
+          "Coffee or tea? I serve both. You're gonna miss me when I'm gone :)",
+      "answer": "Cup"
+    },
+  ],
+};
